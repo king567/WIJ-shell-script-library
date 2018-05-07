1cfb00a26b1501066d4fe11c3f192c0f00da5f8e
WIJTB <104023005@live.asia.edu.tw>
2018-05-08 02:51:56 +0800

    修完大部分了
    可以正常運行 剩下防火牆部分

--- samba.sh
+++ samba.sh
@@ -9,43 +9,56 @@ Bk_conf=$(cp /etc/samba/smb.conf /etc/samba/smb.conf.bak 2> /dev/null)
 Samba_Conf_Path=$(/etc/samba/smb 2> /dev/null)
 Samba_Conf_File=$(/etc/samba/smb.conf 2> /dev/null)
 
-conf_file (){
+Ask_answer (){
+read -p "Please input share folder directory : " share_folder
+read -p "Please input samba explore name : " BIOS_NAME
+read -p "Please input user name : " User_Name
+read -p "Please input samba log max size : " Log_Max_Size
+read -p "Please input samba net folder name : " NET_NAME
+share_folder=${share_folder:="/samba/anonymous"}
+BIOS_NAME=${BIOS_NAME:="WIJTB"}
+User_Name=${User_Name:="king"}
+Log_Max_Size=${Log_Max_Size:="1000"}
+NET_NAME=${NET_NAME:="king"}
+}
+
+Conf_file (){
 conf="[global]
-    dos charset = cp950
-    netbios name = ${BIOS_NAME}
-    server string = %h server (Samba, Ubuntu)
-    server role = standalone server
-    map to guest = Bad User
-    obey pam restrictions = Yes
-    pam password change = Yes
-    passwd program = /usr/bin/passwd %u
-    passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
-    unix password sync = Yes
-    log file = /var/log/samba/log.%m
-    max log size = ${Log_Max_Size}
-    max protocol = SMB3
-    protocol = SMB3
-    server min protocol = SMB2
-    min protocol = SMB2
-    load printers = No
-    dns proxy = No
-    usershare allow guests = Yes
-    panic action = /usr/share/samba/panic-action %d
-    idmap config * : backend = tdb
+dos charset = cp950
+netbios name = ${BIOS_NAME}
+server string = %h server (Samba, Ubuntu)
+server role = standalone server
+map to guest = Bad User
+obey pam restrictions = Yes
+pam password change = Yes
+passwd program = /usr/bin/passwd %u
+passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+unix password sync = Yes
+log file = /var/log/samba/log.%m
+max log size = ${Log_Max_Size}
+max protocol = SMB3
+protocol = SMB3
+server min protocol = SMB2
+min protocol = SMB2
+load printers = No
+dns proxy = No
+usershare allow guests = Yes
+panic action = /usr/share/samba/panic-action %d
+idmap config * : backend = tdb
 
 
 [${NET_NAME}]
-    comment = need share
-    path = ${share_folder}
-    valid users = ${User_Name}
-    force user = ${User_Name}
-    force group = ${User_Name}
-    group = ${User_Name}
-    read only = No
-    create mask = 0777
-    directory mask = 0777
-    directory mode = 0777
-    guest ok = Yes"
+comment = need share
+path = ${share_folder}
+valid users = ${User_Name}
+force user = ${User_Name}
+force group = ${User_Name}
+group = ${User_Name}
+read only = No
+create mask = 0777
+directory mask = 0777
+directory mode = 0777
+guest ok = Yes"
 }
 Check_install (){
 if [ -f "/usr/sbin/smbd" ]; then
@@ -56,7 +69,7 @@ fi
 }
 
 Install_samba (){
-conf_file
+Conf_file
 Check_install
 	if [ $test -eq 1 ]; then
 	echo "installing samba"
@@ -68,7 +81,10 @@ Check_install
 	read -p "Please Input Number (1-2):" choice
 		case ${choice} in
 		1)	
-			apt-get remove --purge -y samba samba-common python-glade2 system-config-samba && apt-get install -y samba samba-common python-glade2 system-config-samba
+			apt-get remove --purge -y samba samba-common python-glade2 system-config-samba 
+			wait
+			apt-get install -y samba samba-common python-glade2 system-config-samba
+			wait
 		;;
 		2)
 			break
@@ -80,23 +96,14 @@ Check_install
 	fi
 wait
 echo -e "\n${COLOR_GREEN}Install Success !!!!${COLOR_REST}\n"
-read -p "Please input share folder directory : " share_folder
-read -p "Please input samba explore name : " BIOS_NAME
-read -p "Please input user name : " User_Name
-read -p "Please input samba log max size : " Log_Max_Size
-read -p "Please input samba net folder name : " NET_NAME
-share_folder=${share_folder:="/samba/anonymous"}
-BIOS_NAME=${BIOS_NAME:="WIJTB"}
-User_Name=${User_Name:="king"}
-Log_Max_Size=${Log_Max_Size:="1000"}
-NET_NAME=${NET_NAME:="king"}
-$conf > $Samba_Conf_File
+
+echo "${conf}" > /etc/samba/smb.conf
 if [ -d "${share_folder}" ]; then
     echo "Directory ${share_folder} exists."
 else
     echo "Directory ${share_folder} does not exists."
 	echo "createing folder...."
-	$mkdir $share_folder
+	mkdir -p $share_folder
 fi
 chmod -R 0770 $share_folder
 chown -R king:king $share_folder
@@ -129,7 +136,7 @@ echo -e "\n${COLOR_GREEN}Starting Samba Service Success !!!${COLOR_REST}\n"
 ListIPcmd="/sbin/ifconfig"
 IP=$($ListIPcmd | grep 'inet addr:' | grep -v '127.0.0.1' | awk '{print $2}' | awk -F:  '{print $2}')
 echo -e "\nthis is windows samba information\n"
-echo -e "\n${COLOR_GREEN}"'  \\''\\'"${IP}\\${NET_NAME}{COLOR_REST}\n"
+echo -e "\n${COLOR_GREEN}"'  \\''\\'"${IP}\\${NET_NAME}${COLOR_REST}\n"
 
 }
 
@@ -142,6 +149,7 @@ echo "(3).Exit"
 read -p "Please Input Number (1-3):" choice
 	case ${choice} in
 	1)	
+		Ask_answer
 		Install_samba
 	;;
 	2)
