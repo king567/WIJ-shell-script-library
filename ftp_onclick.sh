ac42069e43649e276a4c5069ddb54498c6f89818
WIJTB <104023005@live.asia.edu.tw>
2018-06-20 01:18:14 +0800

    add ftp

--- ftp_onclick.sh
+++ ftp_onclick.sh
@@ -75,7 +75,34 @@ echo "(2).被動式"
 read -p "請輸入選項(1-2):" ftp_type
 case ${ftp_type} in
    1)
-		
+		echo "anonymous_enable=NO
+chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd/chroot_list
+chroot_local_user=YES
+connect_from_port_20=NO
+pasv_enable=YES
+anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+local_umask=022
+dirmessage_enable=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+xferlog_std_format=YES
+listen=NO
+listen_ipv6=YES
+pam_service_name=vsftpd
+userlist_enable=YES
+tcp_wrappers=YES
+chroot_local_user=YES
+chroot_list_enable=YES
+chroot_list_file=/etc/vsftpd/chroot_list  
+connect_from_port_20=NO
+pasv_enable=YES" > ${Ubuntu_ftp_conf_path}
+read -p "請輸入使用者名稱：" User_Name
+adduser ${User_Name}
+passwd ${User_Name}
+echo ${User_Name} >> /etc/vsftpd/chroot_list
      ;;
    2)
 		
